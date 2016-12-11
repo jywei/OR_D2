01c13ca412058d935618b6ea8e52a54735ed507d
Roy Wei <roywjy@gmail.com>
2016-12-11 16:41:59 +0800

    user mount instead of match

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blog::Application.routes.draw do
   resources :posts
-  
+
+  # match '/sinatra' => MySinatraApp, anchor: false
+  # mount MySinatraApp, at: '/', anchor: false
   root to: 'posts#index'
 end
